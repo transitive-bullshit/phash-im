8fdfeec968cfae94f01e53f9760bd910bd3454ff
Travis Fischer <fisch0920@gmail.com>
2018-04-04 06:29:26 -0400

    .

--- index.js
+++ index.js
@@ -12,17 +12,15 @@ exports.compute = async (input) => {
 
   // imagemagick uses some non-standard json...
   const stdout = cmd.stdout.replace(/\b(nan)\b/g, 'null').replace(/␍/g, '')
-  console.log(stdout)
-
   const result = JSON.parse(stdout)
 
-  const { image } = result[0]
+  const { image } = (Array.isArray(result) ? result[0] : result)
   const channels = image.channelPerceptualHash
 
   const values = []
-    .concat(extractChannel(channels.Channel0))
-    .concat(extractChannel(channels.Channel1))
-    .concat(extractChannel(channels.Channel2))
+    .concat(extractChannel(channels.Channel0 || channels.redHue))
+    .concat(extractChannel(channels.Channel1 || channels.greenChrome))
+    .concat(extractChannel(channels.Channel2 || channels.blueLuma))
 
   return values
 }
@@ -38,7 +36,7 @@ exports.compare = async (hash1, hash2) => {
 }
 
 function extractChannel (channel) {
-  return [
+  const results = [
     channel.PH1[0],
     channel.PH1[1],
     channel.PH2[0],
@@ -54,4 +52,10 @@ function extractChannel (channel) {
     channel.PH7[0],
     channel.PH7[1]
   ]
+
+  if (typeof results[0] === 'string') {
+    return results.map(parseFloat)
+  } else {
+    return results
+  }
 }
